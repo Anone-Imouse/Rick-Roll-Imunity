92569dd034ddfe59784c968f57fdf71793cf4ff7
Anone-Imouse <73369548+Anone-Imouse@users.noreply.github.com>
2023-11-10 17:54:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const isAYouTubePage = location.href.includes("https://www.youtube.com/")
+const isAYouTubeWatchPage = location.href.includes("https://www.youtube.com/watch") || location.href.includes("https://www.youtube.com/shorts")
+const susCheck = /r(i|1)ckr(o|0)ll|r(i|1)ck( )?(a|4)(s|5)tl(e|3)y|n(e|3)v(e|3)r g(o|0)nn(4|a) g(i|1)v(e|3) y(o|0)u up|nggyu|(a|4)(s|5)tl(e|3)y|r(1|i)ck( )+r(o|0)(l|7)(l|7)|r(1|i)ck(4|a)(s|5)tl(e|3)y/gim
+
 if(document.querySelector('video')){
     if(document.querySelector('video').autoplay){
         document.querySelector('video').pause()
@@ -8,6 +12,16 @@ document.querySelector('video').removeEventListener('click',myFunc)
 document.querySelector('video').play()
   }
  }
+}
+
+if(isAYouTubeWatchPage){
+ let int = setInterval(() => {
+  if(document.querySelector('video')){
+    document.querySelector('video').pause()
+    clearInterval(int)
+    setTimeout(() => document.querySelector('video').play(), 100);
+  }
+ },500)
 }
 
 if(document.querySelector('iframe')){
@@ -28,9 +42,15 @@ setInterval(() => {
 if(location.href === 'https://www.youtube.com/watch?v=dQw4w9WgXcQ' || location.href === 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&feature=youtu.be'){
     location.href = "about:blank"
 }
+if(isAYouTubePage && 
+(location.href.match(susCheck) || 
+(document.querySelector('#title > h1 > yt-formatted-string') ? document.querySelector("#title > h1 > yt-formatted-string").textContent.match(susCheck) : null) ||
+(document.querySelector("#overlay > ytd-reel-player-header-renderer > h2 > yt-formatted-string") ? document.querySelector("#overlay > ytd-reel-player-header-renderer > h2 > yt-formatted-string").textContent.match(susCheck) : null))){
+    location.href = "about:blank"
+}
 },1000)
 
- if(location.href.toLowerCase().indexOf('noordstar') > -1){
+ if(location.href.toLowerCase().includes('noordstar')){
     alert(`NoNoNo!!! This page was bad and tried to rickroll you :(`)
      location.href = "about:blank"
 }
