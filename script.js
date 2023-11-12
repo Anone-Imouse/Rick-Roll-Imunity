55ff2360001b5180525cf154328fe0c91b0f5c58
Anone-Imouse <73369548+Anone-Imouse@users.noreply.github.com>
2023-11-11 22:38:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 if(!(location.href.includes("https://github.com/Anone-Imouse/Rick-Roll-Imunity/"))){
-const isAYouTubePage = location.href.includes("https://www.youtube.com/")
-const isAYouTubeWatchPage = location.href.includes("https://www.youtube.com/watch") || location.href.includes("https://www.youtube.com/shorts")
-const susCheck = /r(i|1)ckr(o|0)ll|r(i|1)ck( )?(a|4)(s|5)tl(e|3)y|n(e|3)v(e|3)r g(o|0)nn(4|a) g(i|1)v(e|3) y(o|0)u up|nggyu|(a|4)(s|5)tl(e|3)y|r(1|i)ck( )+r(o|0)(l|7)(l|7)|r(1|i)ck(4|a)(s|5)tl(e|3)y/gim
+const isAYouTubePage = () => location.href.includes("https://www.youtube.com/");
+const isAYouTubeWatchPage = () => location.href.includes("https://www.youtube.com/watch") || location.href.includes("https://www.youtube.com/shorts");
+const susCheck = /r(i|1)ckr(o|0)(l|7)(l|7)|r(i|1)ck( )?(a|4)(s|5)tl(e|3)y|n(e|3)v(e|3)r g(o|0)nn(4|a) g(i|1)v(e|3) y(o|0)u up|nggyu|(a|4)(s|5)tl(e|3)y|r(1|i)ck( )+r(o|0)(l|7)(l|7)|r(1|i)ck(4|a)(s|5)tl(e|3)y/gim;
 const {log} = console;
+var blockList;
 
     chrome.runtime.sendMessage({ type: 'request' }, response => {
         let start = Date.now();
-        const blockList = response
+        blockList = response
         
         let int = setInterval(() =>{
-            if(isAYouTubeWatchPage){
+            if(isAYouTubeWatchPage()){
                 let videoID = location.search.replace(/&(.)+/g,'').replace(/\?v=/g,'')
                 blockList.forEach(blockedLink => {
                     if(blockedLink.includes(videoID)){
+                        document.querySelector('video').pause()
                         clearInterval(int)
                         let timeTaken = (Date.now() - start) / 1000;
                         log(`It took ${Math.round((timeTaken+Number.EPSILON) * 100) / 100} seconds to successfully redirect you from this rickroll.`)
@@ -20,11 +22,11 @@ const {log} = console;
                     }
                 })
             }
-        },500)
+        },100)
     })
 
 if(document.querySelector('video')){
-    if(document.querySelector('video').autoplay){
+ if(document.querySelector('video').autoplay){
         document.querySelector('video').pause()
         document.querySelector('video').addEventListener('click',myFunc)
         
@@ -41,7 +43,7 @@ document.querySelector('video').play()
             clearInterval(int)
             let list = []
             document.querySelectorAll('a').forEach(basicLink => {
-                let link = basicLink.href
+                let link = basicLink.href.replace(/&(.)+/g,'')
                 if(link.includes('/shorts') || link.includes('/watch')){
                     list.push(link)
                 }
@@ -54,9 +56,10 @@ document.querySelector('video').play()
     },300)
 }*/
 
-if(isAYouTubeWatchPage){
+setInterval(() => {
+if(isAYouTubeWatchPage()){
  let int = setInterval(() => {
-  if(document.querySelector('video')){
+  if(blockList.join(' ').includes(location.href.replace(/&(.)+/g,''))){
     document.querySelector('video').pause()
     clearInterval(int)
     let int1 = setInterval(() => {
@@ -68,6 +71,7 @@ if(isAYouTubeWatchPage){
   }
  },100)
 }
+},10)
 
 if(document.querySelector('iframe')){
     if(document.querySelector('iframe').src == "https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ?&enablejsapi=1&autoplay=1&mute=1"){
@@ -75,7 +79,7 @@ document.querySelector('iframe').remove()
 }
 }
 
-if(location.href.toLowerCase().indexOf('rick') > -1 || location.href.toLowerCase().indexOf('astley') > -1 || location.href.toLowerCase().indexOf('roll') > -1 ){
+if(location.href.match(susCheck)){
    document.body.innerHTML = `
         <h1>NoNoNo!!!</h1>
   <p>This page was bad and tried to rickroll you :(</p>
@@ -83,11 +87,7 @@ if(location.href.toLowerCase().indexOf('rick') > -1 || location.href.toLowerCase
 }
 
 setInterval(() => {
-    
-if(location.href === 'https://www.youtube.com/watch?v=dQw4w9WgXcQ' || location.href === 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&feature=youtu.be'){
-    location.href = "about:blank"
-}
-if(isAYouTubePage && 
+if(isAYouTubePage() && 
 (location.href.match(susCheck) || 
 (document.querySelector('#title > h1 > yt-formatted-string') ? document.querySelector("#title > h1 > yt-formatted-string").textContent.match(susCheck) : null) ||
 (document.querySelector("#overlay > ytd-reel-player-header-renderer > h2 > yt-formatted-string") ? document.querySelector("#overlay > ytd-reel-player-header-renderer > h2 > yt-formatted-string").textContent.match(susCheck) : null) ||
@@ -99,5 +99,5 @@ if(isAYouTubePage &&
  if(location.href.toLowerCase().includes('noordstar')){
     alert(`NoNoNo!!! This page was bad and tried to rickroll you :(`)
      location.href = "about:blank"
-}
+ }
 }
